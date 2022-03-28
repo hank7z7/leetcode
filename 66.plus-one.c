029549e8f0a12705cc9f11f0296b71c2d47aedd7
Hank Hsieh <ghank7z7@gmail.com>
2022-03-28 22:12:39 +0800

    Add 66 plus-one and AC

---
+++ 66.plus-one.c
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=66 lang=c
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *plusOne(int *digits, int digitsSize, int *returnSize)
+{
+    int carry = 1;
+    int *result = (int *)malloc((digitsSize + 1) * sizeof(int));
+    int i;
+    for (i = digitsSize - 1; i >= 0; i--)
+    {
+        if ((digits[i] + carry) == 10)
+        {
+            result[i + 1] = 0;
+            carry = 1;
+        }
+        else
+        {
+            result[i + 1] = digits[i] + carry;
+            carry = 0;
+        }
+    }
+
+    if (carry == 0)
+    {
+        *returnSize = digitsSize;
+        return ++result;
+    }
+    else
+    {
+        *returnSize = digitsSize + 1;
+        result[0] = 1;
+        return result;
+    }
+}
+// @lc code=end
+
+// Accepted
+// 111/111 cases passed (8 ms)
+// Your runtime beats 9.85 % of c submissions
+// Your memory usage beats 29.67 % of c submissions (6 MB)
