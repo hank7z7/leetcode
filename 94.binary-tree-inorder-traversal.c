26c365c39c2611c3081b306ac712cba02178277e
hank7z7 <ghank7z7@gmail.com>
2022-07-22 16:29:28 +0800

    Clang-format 94

--- 94.binary-tree-inorder-traversal.c
+++ 94.binary-tree-inorder-traversal.c
@@ -14,27 +14,23 @@
  * };
  */
 
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
-void traversal(struct TreeNode* root, int* array, int* returnSize)
-{
-    if(root)
-    {
+void traversal(struct TreeNode *root, int *array, int *returnSize) {
+    if (root) {
         traversal(root->left, array, returnSize);
-        array[(*returnSize)++] = root->val; // AddressSantizer: () of pointer is needed!
+        array[(*returnSize)++] =
+            root->val; // AddressSantizer: () of pointer is needed!
         traversal(root->right, array, returnSize);
     }
 }
 
-
-int* inorderTraversal(struct TreeNode* root, int* returnSize)
-{
-    int* result = (int*) malloc(sizeof(int) * 100);
+int *inorderTraversal(struct TreeNode *root, int *returnSize) {
+    int *result = (int *)malloc(sizeof(int) * 100);
     *returnSize = 0;
-    
+
     // Recusion method
     traversal(root, result, returnSize);
     result = realloc(result, (*returnSize) * sizeof(int));
