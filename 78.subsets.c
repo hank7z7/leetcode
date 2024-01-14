7e81fe841e3a0ad18f9a9022e71550b460d6bf45
Hank Hsieh <ghank7z7@gmail.com>
2024-01-15 00:19:18 +0800

    format

--- 78.subsets.c
+++ 78.subsets.c
@@ -15,34 +15,34 @@
  *
  * Given an integer array nums of unique elements, return all possible subsets
  * (the power set).
- * 
+ *
  * The solution set must not contain duplicate subsets. Return the solution in
  * any order.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: nums = [1,2,3]
  * Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: nums = [0]
  * Output: [[],[0]]
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= nums.length <= 10
  * -10 <= nums[i] <= 10
  * All the numbers of nums are unique.
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -53,31 +53,29 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int **
-subsets (int *nums, int numsSize, int *returnSize, int **returnColumnSizes)
+int** subsets(int* nums, int numsSize, int* returnSize, int** returnColumnSizes)
 {
-  *returnSize = 1 << numsSize;
-  int **result = (int **) malloc (sizeof (int *) * (*returnSize));
-  *returnColumnSizes = (int *) calloc (*returnSize, sizeof (int));
+    *returnSize        = 1 << numsSize;
+    int** result       = (int**) malloc(sizeof(int*) * (*returnSize));
+    *returnColumnSizes = (int*) calloc(*returnSize, sizeof(int));
 
-  for (int i = 0; i < *returnSize; i++)
+    for (int i = 0; i < *returnSize; i++)
     {
-      (*returnColumnSizes)[i] = 0;
-      int index = 1;
-      result[i] = (int *) malloc (sizeof (int) * numsSize);
-      // Every bits in j means whether nums[i] is in set
-      for (int j = 0; j < numsSize; j++)
-	{
-	  if (((index << j) & i) != 0)
-	    {
-	      (*returnColumnSizes)[i]++;
-	      result[i][(*returnColumnSizes)[i] - 1] = nums[j];
-	    }
-
-	}
+        (*returnColumnSizes)[i] = 0;
+        int index               = 1;
+        result[i]               = (int*) malloc(sizeof(int) * numsSize);
+        // Every bits in j means whether nums[i] is in set
+        for (int j = 0; j < numsSize; j++)
+        {
+            if (((index << j) & i) != 0)
+            {
+                (*returnColumnSizes)[i]++;
+                result[i][(*returnColumnSizes)[i] - 1] = nums[j];
+            }
+        }
     }
 
-  return result;
+    return result;
 }
 
 // @lc code=end
