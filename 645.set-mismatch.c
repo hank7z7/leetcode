5fad922ebcfaea94448a38dc77131ffb53ef6d8d
Hank Hsieh <ghank7z7@gmail.com>
2024-01-22 21:48:42 +0800

    [Daily] 1/22 645 set mismatch

--- 645.set-mismatch.c
+++ 645.set-mismatch.c
@@ -3,21 +3,23 @@
  *
  * [645] Set Mismatch
  */
-#include <bits/stdc++.h>
-// @lc code=start
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int cmp(const void *a, const void *b)
-{
-	return *(int *)a > *(int *)b;
-}
+#define SIZE_1 6
+// @lc code=start
+#define RETRUN_SIZE 2
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int *findErrorNums(int *nums, int numsSize, int *returnSize)
 {
-	(*returnSize) = 2;
-	int *result = (int *)malloc(sizeof(int) * 2);
+	(*returnSize) = RETRUN_SIZE;
+	int *result = (int *)malloc(sizeof(int) * RETRUN_SIZE);
+
+	// Counting method
 	int table[numsSize];
 	memset(table, 0, sizeof(int) * numsSize);
 
@@ -29,7 +31,10 @@ int *findErrorNums(int *nums, int numsSize, int *returnSize)
 	}
 	for (int i = 0; i < numsSize; i++) {
 		if (table[i] == 0)
+		{
 			result[1] = i + 1;
+			break;
+		}
 	}
 
 	return result;
@@ -39,16 +44,18 @@ int *findErrorNums(int *nums, int numsSize, int *returnSize)
 
 int main()
 {
-	int input_size = 6;
-	int arr[input_size] = { 3, 2, 3, 4, 6, 5 };
+	int input_size = SIZE_1;
+	int arr[SIZE_1] = { 3, 2, 3, 4, 6, 5 };
 	int *ans;
 	int returnSize;
 	ans = findErrorNums(arr, input_size, &returnSize);
 	printf("%d %d\n", ans[0], ans[1]);
+	free(ans);
+
 	return 0;
 }
 
 // Accepted
-// 49/49 cases passed (55 ms)
-// Your runtime beats 18.75 % of c submissions
-// Your memory usage beats 76.56 % of c submissions (7.1 MB)
+// 49/49 cases passed (19 ms)
+// Your runtime beats 85.71 % of c submissions
+// Your memory usage beats 100 % of c submissions (6.7 MB)
