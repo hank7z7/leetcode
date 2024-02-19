27b469f4cd4ca5019cc30a14e28a276045ce77b7
Hank Hsieh <ghank7z7@gmail.com>
2024-02-19 16:22:11 +0800

    [Daily 24/02/19] Power of two add driver

--- 231.power-of-two.c
+++ 231.power-of-two.c
@@ -3,7 +3,8 @@
  *
  * [231] Power of Two
  */
-
+#include <stdio.h>
+#include <stdbool.h>
 // @lc code=start
 
 
@@ -15,7 +16,21 @@ bool isPowerOfTwo(int n)
     return !(n & (n - 1));
 }
 // @lc code=end
+int main(int argc, char** argv)
+{
+    int n;
+    // Test_1
+    n = 1;
+    printf("%d\n", isPowerOfTwo(n));
 
+    // Test_1
+    n = 16;
+    printf("%d\n", isPowerOfTwo(n));
+
+    // Test_1
+    n = 13;
+    printf("%d\n", isPowerOfTwo(n));
+}
 // Accepted
 // 1108/1108 cases passed (0 ms)
 // Your runtime beats 100 % of c submissions
--- 231.power-of-two.cpp
+++ 231.power-of-two.cpp
@@ -3,6 +3,7 @@
  *
  * [231] Power of Two
  */
+#include <bits/stdc++.h>
 
 // @lc code=start
 class Solution
@@ -15,6 +16,23 @@ public:
         return (((n) & (n - 1)) == 0);
     }
 };
+int main(int argc, char** argv)
+{
+    int n;
+    Solution obj;
+    // Test_1
+    n = 1;
+    std::cout << obj.isPowerOfTwo(n) << std::endl;
+
+    // Test_2
+    n = 16;
+    std::cout << obj.isPowerOfTwo(n) << std::endl;
+
+    // Test_3
+    n = 3;
+    std::cout << obj.isPowerOfTwo(n) << std::endl;
+    return 0;
+}
 // @lc code=end
 // Accepted
 // 1108/1108 cases passed (0 ms)
