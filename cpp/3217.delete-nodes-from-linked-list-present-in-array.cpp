8989cd2758712d6641c2dd85256d3ebd39754a7a
Hank Hsieh <ghank7z7@gmail.com>
2024-09-08 12:20:03 +0800

    Format 3217

--- cpp/3217.delete-nodes-from-linked-list-present-in-array.cpp
+++ cpp/3217.delete-nodes-from-linked-list-present-in-array.cpp
@@ -79,23 +79,11 @@ using namespace std;
 
 struct ListNode
 {
-    int       val;
-    ListNode* next;
-    ListNode()
-    : val(0)
-    , next(NULL)
-    {
-    }
-    ListNode(int x)
-    : val(x)
-    , next(NULL)
-    {
-    }
-    ListNode(int x, ListNode* next)
-    : val(x)
-    , next(next)
-    {
-    }
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 // @lc code=start
 /**
@@ -111,47 +99,151 @@ struct ListNode
 class Solution
 {
 public:
-    ListNode* modifiedList(vector<int>& nums, ListNode* head)
+    ListNode *modifiedList(vector<int> &nums, ListNode *head)
     {
         unordered_set<int> nums_set;
         for (auto num : nums)
             nums_set.insert(num);
-        // Handle the case where the head node needs to be removed
-        while (head != NULL && nums_set.count(head->val) > 0)
-        {
-            ListNode* temp = head;
-            head           = head->next;
-            delete temp;
-        }
 
-        // If the list is empty after removing head nodes, return NULL
-        if (head == NULL)
-        {
-            return NULL;
-        }
+        // Create a dummy head;
+        ListNode dummy_node = ListNode(0, head);
+        ListNode *prev_node = &dummy_node;
+        ListNode *curr_node = head;
+        ListNode *next_node = nullptr;
 
-        // Iterate through the list, removing nodes with values in the set
-        ListNode* current = head;
-        while (current->next != NULL)
+        while (curr_node)
         {
-            if (nums_set.find(current->next->val) != nums_set.end())
+            next_node = curr_node->next;
+
+            if (nums_set.find(curr_node->val) != nums_set.end())
             {
-                // Store the node to be deleted
-                ListNode* temp = current->next;
-                // Skip the next node by updating the pointer
-                current->next = current->next->next;
-                // Delete the removed node
-                delete temp;
+                prev_node->next = next_node;
+                delete curr_node;
+                curr_node = next_node;
             }
             else
             {
-                // Move to the next node
-                current = current->next;
+                prev_node = curr_node;
+                curr_node = next_node;
             }
         }
 
-        return head;
+        return dummy_node.next;
     }
 };
 // @lc code=end
-int main(int argc, char** argv) { return 0; }
+
+bool areSame(ListNode *a, ListNode *b)
+{
+    // Traverse both lists
+    while (a && b)
+    {
+        // If values at the current nodes are different, return false
+        if (a->val != b->val)
+        {
+            return false;
+        }
+        // Move to the next nodes
+        a = a->next;
+        b = b->next;
+    }
+
+    // If both lists have reached the end (nullptr), return true
+    // Otherwise, one list is longer than the other, return false
+    return a == nullptr && b == nullptr;
+}
+
+int main(int argc, char **argv)
+{
+    Solution sol;
+    // Test_1
+    vector<int> nums_1 = {1, 2, 3};
+    ListNode *test_1_node_4 = new ListNode(5);
+    ListNode *test_1_node_3 = new ListNode(4, test_1_node_4);
+    ListNode *test_1_node_2 = new ListNode(3, test_1_node_3);
+    ListNode *test_1_node_1 = new ListNode(2, test_1_node_2);
+    ListNode *test_1_node_0 = new ListNode(1, test_1_node_1);
+    ListNode *res_1 = sol.modifiedList(nums_1, test_1_node_0);
+    ListNode *curr = res_1;
+    while (curr)
+    {
+        cout << curr->val << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+
+    // Check the answer
+    ListNode ans_1_node_1 = ListNode(5);
+    ListNode ans_1_node_0 = ListNode(4, &ans_1_node_1);
+    assert(areSame(res_1, &ans_1_node_0));
+
+    // Delete result
+    curr = res_1;
+    while (curr)
+    {
+        ListNode *d = curr;
+        curr = curr->next;
+        delete d;
+    }
+
+    // Test_2
+    vector<int> nums_2 = {1};
+    ListNode *test_2_node_5 = new ListNode(2);
+    ListNode *test_2_node_4 = new ListNode(1, test_2_node_5);
+    ListNode *test_2_node_3 = new ListNode(2, test_2_node_4);
+    ListNode *test_2_node_2 = new ListNode(1, test_2_node_3);
+    ListNode *test_2_node_1 = new ListNode(2, test_2_node_2);
+    ListNode *test_2_node_0 = new ListNode(1, test_2_node_1);
+    ListNode *res_2 = sol.modifiedList(nums_2, test_2_node_0);
+    ListNode *curr_2 = res_2;
+    while (curr_2)
+    {
+        cout << curr_2->val << " ";
+        curr_2 = curr_2->next;
+    }
+    cout << endl;
+
+    // Check the answer
+    ListNode ans_2_node_2 = ListNode(2);
+    ListNode ans_2_node_1 = ListNode(2, &ans_2_node_2);
+    ListNode ans_2_node_0 = ListNode(2, &ans_2_node_1);
+    assert(areSame(res_2, &ans_2_node_0));
+
+    // Delete result
+    curr_2 = res_2;
+    while (curr_2)
+    {
+        ListNode *d = curr_2;
+        curr_2 = curr_2->next;
+        delete d;
+    }
+
+    // Test_3
+    vector<int> nums_3 = {1, 2, 3, 4};
+    ListNode *test_3_node_3 = new ListNode(4);
+    ListNode *test_3_node_2 = new ListNode(3, test_3_node_3);
+    ListNode *test_3_node_1 = new ListNode(2, test_3_node_2);
+    ListNode *test_3_node_0 = new ListNode(1, test_3_node_1);
+    ListNode *res_3 = sol.modifiedList(nums_3, test_3_node_0);
+    ListNode *curr_3 = res_3;
+    while (curr_3)
+    {
+        cout << curr_3->val << " ";
+        curr_3 = curr_3->next;
+    }
+    cout << endl;
+
+    // Check the answer
+    assert(areSame(res_3, nullptr));
+
+    // Delete result
+    curr_3 = res_3;
+    while (curr_3)
+    {
+        ListNode *d = curr_3;
+        curr_3 = curr_3->next;
+        delete d;
+    }
+
+    return 0;
+}
