5b5e5932314d602b2292fa64fb30b3cef5f376e6
Hank Hsieh <ghank7z7@gmail.com>
2024-10-05 02:08:54 +0800

    [Daily 24/10/05] 2491

--- cpp/2491.divide-players-into-teams-of-equal-skill.cpp
+++ cpp/2491.divide-players-into-teams-of-equal-skill.cpp
@@ -73,57 +73,81 @@ using namespace std;
 class Solution
 {
 public:
-    long long dividePlayers(vector<int>& skill)
+    long long dividePlayers(vector<int> &skill)
     {
-        const int n = (int) skill.size();
+        const int n = (int)skill.size();
         const int teams = n / 2;
-        // skill -> count
+        // num -> count
         unordered_map<int, int> um;
         int total_sum = 0;
-        for (int i = 0; i < n; i++)
+        for (auto num : skill)
         {
-            total_sum += skill[i];
-            um[skill[i]]++;
+            total_sum += num;
+            um[num]++;
         }
-        const int team_skill = total_sum / teams;
+        // Check if cannot divide to teams
+        if (total_sum % teams)
+            return -1;
+        const int pair_sum = total_sum / teams;
 
         long long res = 0;
         for (auto it : um)
         {
-            if( it.second != um[team_skill - it.first])
+            int pair_1 = it.first;
+            int count_1 = it.second;
+            int pair_2 = pair_sum - pair_1;
+            int count_2 = um[pair_2];
+            if (count_1 != count_2)
                 return -1;
-            if (it.second > 0 && it.second == um[team_skill - it.first])
-            {
-                cout << it.first << " " << team_skill - it.first << endl;
-                res += ((long long) it.second * (long long) it.first * (long long) (team_skill - it.first));
-                it.second = 0;
-                um[team_skill - it.first] = 0;
-            }
+            if (count_1 == 0 || count_2 == 0)
+                continue;
+            else if (pair_1 == pair_2)
+                res += ((long long)count_1 / 2 * (long long)pair_1 * (long long)pair_2);
+            else
+                res += ((long long)count_1 * (long long)pair_1 * (long long)pair_2);
+            um[pair_1] = 0;
+            um[pair_2] = 0;
         }
         return res;
     }
 };
 // @lc code=end
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     Solution sol;
     // Test_1
-    vector<int> skill_1 = { 3, 2, 5, 1, 3, 4 };
+    vector<int> skill_1 = {3, 2, 5, 1, 3, 4};
     int res_1 = sol.dividePlayers(skill_1);
     cout << res_1 << endl;
     assert(res_1 == 22);
 
     // Test_2
-    vector<int> skill_2 = { 3, 4 };
+    vector<int> skill_2 = {3, 4};
     int res_2 = sol.dividePlayers(skill_2);
     cout << res_2 << endl;
     assert(res_2 == 12);
 
     // Test_3
-    vector<int> skill_3 = { 1, 1, 2, 3 };
+    vector<int> skill_3 = {1, 1, 2, 3};
     int res_3 = sol.dividePlayers(skill_3);
     cout << res_3 << endl;
     assert(res_3 == -1);
 
+    // Test_4
+    vector<int> skill_4 = {5, 1, 3, 6};
+    int res_4 = sol.dividePlayers(skill_4);
+    cout << res_4 << endl;
+    assert(res_4 == -1);
+
+    // Test_5
+    vector<int> skill_5 = {2, 1, 5, 2};
+    int res_5 = sol.dividePlayers(skill_5);
+    cout << res_5 << endl;
+    assert(res_5 == -1);
+
     return 0;
 }
+// Accepted
+// 88/88 cases passed (56 ms)
+// Your runtime beats 87 % of cpp submissions
+// Your memory usage beats 14.6 % of cpp submissions (57.2 MB)
